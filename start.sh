41e71924b7ce5d1db58bc83ae00c582d43827392
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-09-10 18:22:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ echo "start.sh started at $(date)" >> log/start.log
 
 # Endlosschleife
 while true; do
-    ../venv/bin/python3 main.py >> log/$(date +"%Y-%m-%d_%H-%M-%S").log 2>&1
+    ../venv/bin/python3 main.py >> log/$(date +"%Y-%m-%d_%H-%M-%S").log 2>&1 || venv/bin/python3 main.py >> log/$(date +"%Y-%m-%d_%H-%M-%S").log 2>&1 
     sleep 60
 done
